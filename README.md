2719c28460d108aeefe02fade1316286aeccc19c
Jeremy Hitchcock <whatthefuture@gmail.com>
2019-10-29 14:58:14 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# generative-art
-
-Uses [parcel](https://parceljs.org/getting_started.html)
-
-to run:
-
-`parcel index.html`
--- src/utils/lineFunctions.js
+++
@@ -1,5 +0,0 @@
-export const getSlope = (start, end) =>
-  (end.y - start.y) / (end.x - start.x);
-
-export const getYIntercept = (start, slope) => 
-  start.y - (slope * start.x); 
